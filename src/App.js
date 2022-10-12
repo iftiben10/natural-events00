bae5889711646b1f289b3277852199a6f522c778
Yusuf Iqbal Ifti <78135858+iftiben10@users.noreply.github.com>
2022-10-12 17:01:34 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,4 +38,9 @@ class App extends React.Component {
   };
 };
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
+
 export default App;
